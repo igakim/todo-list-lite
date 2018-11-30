66c08598a0f6521cf6c0141cd12cf29263b46067
Igor Kim <igkim90@gmail.com>
2018-11-30 18:45:04 +0500

    add container for editForm

---
+++ src/js/containers/EditFormContainer.js
@@ -0,0 +1,7 @@
+import { connect } from 'react-redux';
+import * as actionCreators from '../actions';
+import EditForm from '../components/EditForm.jsx';
+
+const mapStateToProps = () => ({});
+
+export default connect(mapStateToProps, actionCreators)(EditForm);
